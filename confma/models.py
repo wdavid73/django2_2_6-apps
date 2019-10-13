1285e99e08719039974f37779ac2f8464570ab2c
Wilson Padilla <wpadilla4@cuc.edu.co>
2019-10-13 16:30:20 -0500

    model users v1

--- confma/models.py
+++ confma/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name        = models.CharField(),
+    lastname    = models.CharField(),
+    address     = models.CharField(),
+    phone       = models.IntegerField(),
+    cellphone   = models.IntegerField(),
+    state       = models.BinaryField(),
+    ##########################################3
+    created_at  = models.DateTimeField(auto_now_add=True),
+    updated_at  = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        db_table = 'confma_users'
