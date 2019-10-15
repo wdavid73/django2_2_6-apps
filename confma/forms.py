b64ebe0cb84de940e11b07cf16784e7c376d70a3
Wilson Padilla <wpadilla4@cuc.edu.co>
2019-10-14 19:09:10 -0500

    update finish

--- confma/forms.py
+++ confma/forms.py
@@ -2,6 +2,24 @@ from django.forms import ModelForm , TextInput
 from django import forms
 from .models import User
 
+class UserFormModel(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'name',
+            'lastname',
+            'address',
+            'phone',
+            'cellphone'
+        ]
+        widgets = {
+            'name' : forms.TextInput( attrs = {'placeholder' : 'Your Name' , 'class' : 'form-control'}),
+            'lastname' : forms.TextInput( attrs = {'placeholder' : 'Your LastName' , 'class' : 'form-control'}),
+            'address' : forms.TextInput( attrs = {'placeholder' : 'Your Address' , 'class' : 'form-control'}),
+            'phone' : forms.NumberInput( attrs = {'placeholder' : 'Your Phone' , 'class' : 'form-control'}),
+            'cellphone' : forms.NumberInput( attrs = {'placeholder' : 'Your CellPhone' , 'class' : 'form-control'})
+        }
+
 class UserForm(forms.Form):
     name = forms.CharField( max_length=100,
                             # help_text='Use puns liberally',
