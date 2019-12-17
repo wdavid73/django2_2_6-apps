5abdb8f7fdbf20e130167d6edf1e7cf1bcb5ee48
Wilson Padilla <wpadilla4@cuc.edu.co>
2019-12-17 17:24:37 -0500

    validacion de todos los formulario v1

--- confma/forms/signup.py
+++ confma/forms/signup.py
@@ -4,9 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
 
-	first_name = forms.CharField()
-	last_name = forms.CharField()
-	email = forms.CharField()
+	first_name = forms.CharField(label = "Insert Your First Name",required=False)
+	last_name = forms.CharField(label = "Insert Your Last Name",required=False)
+	email = forms.CharField(label = "Insert a e-mail",required=False)
 
 	class Meta(UserCreationForm.Meta):
 		model = User
