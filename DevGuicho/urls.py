56aa2de896a98d4c5cda4784b40ecfb3aba162a7
Wilson Padilla <wpadilla4@cuc.edu.co>
2020-02-24 18:27:48 -0500

    modificaciones 2

--- DevGuicho/urls.py
+++ DevGuicho/urls.py
@@ -1,41 +1,11 @@
 from django.conf import settings
 from django.conf.urls import url
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('confma.urls_app', namespace="confma")),
     path('accounts/', include('django.contrib.auth.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
-
-# urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += staticfiles_urlpatterns()
-#
-# if settings.DEBUG is False and settings.SANDBOX is True:
-#     urlpatterns += patterns('',
-#         url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
-#             {'document_root': settings.STATIC_ROOT}),
-#         url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-#             {'document_root': settings.MEDIA_ROOT}),
-#     )
-#
-# urlpatterns += patterns(
-#     '',
-#     url(r'^', include('cms.urls')),
-# )
-
-
-# if settings.DEBUG is True:
-#     urlpatterns += staticfiles_urlpatterns()
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#
-# urlpatterns += staticfiles_urlpatterns()
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# urlpatterns = [
-#     path('', views.index, name="index"),
-#     path('confma/', include('confma.urls', namespace="confma")),
-#     path('accounts/', include('django.contrib.auth.urls')),
-# ]
